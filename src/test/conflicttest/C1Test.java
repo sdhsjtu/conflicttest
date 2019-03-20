a121ce80cb69c37bd08f4ceda91038934eeafe38
sdh <251287012@qq.com>
2019-03-20 15:18:30 +0800

    sdh invoke C1.equal()

--- src/test/conflicttest/C1Test.java
+++ src/test/conflicttest/C1Test.java
@@ -8,13 +8,13 @@ import com.C1;
 
 public class C1Test {
 
-//	@Test
-//	public void testEqual() {
-//		C1 c1 = new C1();
-//		Integer i = 1;
-//		Integer j = new Integer(1);
-//		boolean ans = c1.equal(i,j);
-//		assertEquals(true, ans);
-//	}
+	@Test
+	public void testEqual() {
+		C1 c1 = new C1();
+		Integer i = 1;
+		Integer j = new Integer(1);
+		boolean ans = c1.equal(i,j);
+		assertEquals(true, ans);
+	}
 
 }
