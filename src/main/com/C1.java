7417f91bd9e8b760993cae6a869a9b6882ed857d
sdh <251287012@qq.com>
2019-03-20 13:26:12 +0800

    m

--- src/main/com/C1.java
+++ src/main/com/C1.java
@@ -1,18 +1,14 @@
 package com;
 
 import shape.Circle;
-import shape.Shape;
-import shape.Square;
 
 public class C1 {
-	private Shape s = new Square(1);
-	
 	public int addI(int j) {
 		System.out.println("function addI");
 		return j+2;
 	}
 	
-	public double getAllLength(int j) {
-		return s.getLength(j);
+	public double getAllLength(Circle s) {
+		return 0;
 	}
 }
