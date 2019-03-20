25e329ea49e8fcad73962d6af6f735cf54e394ec
userA <wudawu2@163.com>
2019-03-20 14:36:00 +0800

    userA import java.util.List

--- src/main/com/C1.java
+++ src/main/com/C1.java
@@ -2,6 +2,9 @@ package com;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import shape.Circle;
 
 public class C1 {
@@ -11,7 +14,7 @@ public class C1 {
 	}
 
 	public double getAllLength(Circle s,int i) {
-//		List l = new ArrayList();
+		List l = new ArrayList();
 		for(int k=0;k<10;++k) {
 			int j=0;
 		}
