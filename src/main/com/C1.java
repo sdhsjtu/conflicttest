bd6943fdddf8cdaafc3da2767a8b0d47d0a323f5
sdh <251287012@qq.com>
2019-03-20 14:46:41 +0800

    m

--- src/main/com/C1.java
+++ src/main/com/C1.java
@@ -1,24 +1,17 @@
 package com;
 
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 import shape.Circle;
-import java.awt.List;
 public class C1 {
 	public int addI(int j) {
 		System.out.println("function addI");
 		return j+2;
 	}
 
-	public double getAllLength(Circle s,int i) {
-		List l = new ArrayList();
+	public static double getAllLength(Circle s,int i) {
 		for(int k=0;k<10;++k) {
 			int j=0;
 		}
-		List list;
 		return 0;
 	}
 }
