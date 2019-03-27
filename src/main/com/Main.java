48b5d34df4b0df2599091d898743280ce52c4c60
sdh <251287012@qq.com>
2019-03-27 20:45:46 +0800

    m2

--- src/main/com/Main.java
+++ src/main/com/Main.java
@@ -1,21 +1,9 @@
 package com;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) {
-		List<Integer> l2 = Arrays.asList(2,3,1,4);
-		List<Integer> l = new ArrayList<Integer>();
-		l.addAll(l2);
-		l.remove(new Integer(1));
-		for(int i:l) {
-			System.out.println(i);
-		}
-		
-		
+
 	}
 
 }
