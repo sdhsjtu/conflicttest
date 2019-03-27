6fbbba91df08aa121951d4e6fc95c5536b193d0c
sdh <251287012@qq.com>
2019-03-27 20:54:28 +0800

    m

--- src/test/conflicttest/C1Test.java
+++ src/test/conflicttest/C1Test.java
@@ -1,20 +1,14 @@
 package conflicttest;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import com.C1;
-
 public class C1Test {
 
-	@Test
-	public void testEqual() {
-		C1 c1 = new C1();
-		Integer i = 1;
-		Integer j = new Integer(1);
-		boolean ans = c1.equal(i,j);
-		assertEquals(true, ans);
-	}
+//	@Test
+//	public void testEqual() {
+//		C1 c1 = new C1();
+//		Integer i = 1;
+//		Integer j = new Integer(1);
+//		boolean ans = c1.equal(i,j);
+//		assertEquals(true, ans);
+//	}
 
 }
