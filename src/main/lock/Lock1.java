f771d607cae4db31405f5e91c1644bb9b4b75959
userA <wudawu2@163.com>
2019-03-27 23:49:50 +0800

    m

--- src/main/lock/Lock1.java
+++ src/main/lock/Lock1.java
@@ -3,20 +3,20 @@ package lock;
 public class Lock1 implements Runnable {
 
 	public void run() {
-		try{
-            System.out.println("Lock1 running");
-            while(true){
-                synchronized(DeadLock.obj1){
-                    System.out.println("Lock1 lock obj1");
-                    Thread.sleep(1000);
-                    synchronized(DeadLock.obj2){
-                        System.out.println("Lock1 lock obj2");
-                    }
-                }
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+//		try{
+//            System.out.println("Lock1 running");
+//            while(true){
+//                synchronized(DeadLock.obj1){
+//                    System.out.println("Lock1 lock obj1");
+//                    Thread.sleep(1000);
+//                    synchronized(DeadLock.obj2){
+//                        System.out.println("Lock1 lock obj2");
+//                    }
+//                }
+//            }
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
 	}
 
 }
