369ddadea571004aa4e80365988ea1ae4e740bc4
sdh <251287012@qq.com>
2019-03-27 23:47:53 +0800

    m

---
+++ src/main/lock/Lock2.java
@@ -0,0 +1,22 @@
+package lock;
+
+public class Lock2 implements Runnable {
+
+	public void run() {
+//		try{
+//            System.out.println("Lock2 running");
+//            while(true){
+//                synchronized(DeadLock.obj2){
+//                    System.out.println("Lock2 lock obj2");
+//                    Thread.sleep(1000);
+//                    synchronized(DeadLock.obj1){
+//                        System.out.println("Lock2 lock obj1");
+//                    }
+//                }
+//            }
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+	}
+
+}
