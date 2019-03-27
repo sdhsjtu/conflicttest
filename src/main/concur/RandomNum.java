d627ef2b05dd25f1d3a7d75ea934ca729fa7d448
sdh <251287012@qq.com>
2019-03-28 01:01:11 +0800

    sdh uses vector and always gets the right answer

--- src/main/concur/RandomNum.java
+++ src/main/concur/RandomNum.java
@@ -21,5 +21,27 @@ public class RandomNum implements Runnable {
 		list.add(Thread.currentThread().getName() + "----------------" + random.nextInt(100));
 	}
 
+	public static void main(String[] args) throws InterruptedException {
+		RandomNum randomNum = new RandomNum();
+		Thread thread1 = new Thread(randomNum);
+		Thread thread2 = new Thread(randomNum);
+		Thread thread3 = new Thread(randomNum);
+		Thread thread4 = new Thread(randomNum);
+
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		thread4.start();
+
+		thread1.join();
+		thread2.join();
+		thread3.join();
+		thread4.join();
+
+		for (String str : randomNum.getList()) {
+			System.out.println(str);
+		}
+
+	}
 
 }
