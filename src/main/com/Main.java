9183046fdf0808223a29787cb6203e6c67324b23
sdh <251287012@qq.com>
2019-03-27 23:00:23 +0800

    m

--- src/main/com/Main.java
+++ src/main/com/Main.java
@@ -1,14 +1,16 @@
 package com;
 
 public class Main {
+	public static String s(String name) {
+
+		/*
+		 * 
+		 */
+
+        return null;     
+	}
 
 	public static void main(String[] args) {
-		String s1 = null;
-		String s2 = "str";
-		System.out.println(s1);
-		if(s1.equals(s2)) {
-			System.out.println("equal");
-		}
 	}
 
 }
