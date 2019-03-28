5763b7eba5d013fbc2dcbde6a4812764fb2f51e5
sdh <251287012@qq.com>
2019-03-28 12:52:40 +0800

    m

--- src/main/com/C1.java
+++ src/main/com/C1.java
@@ -1,21 +1,6 @@
 package com;
 
 
-import shape.Circle;
 public class C1 {
-	public int addI(int j) {
-		System.out.println("function addI");
-		return j+2;
-	}
-
-	public static double getAllLength(Circle s,int i) {
-		for(int k=0;k<10;++k) {
-			int j=0;
-		}
-		return 0;
-	}
-	
-	public static boolean equal(int i,int j) {
-		return i==j;
-	}
+	public double f = 3.3f;
 }
--- src/main/com/Main.java
+++ src/main/com/Main.java
@@ -1,24 +1,9 @@
 package com;
 
 public class Main {
-	public static String s(String name) {
-		boolean flag1 = true;
-		/*
-		 * 
-		 */
-		boolean flag2 = true;
-		while(flag1 && flag2) {
-			System.out.println("flag is true");
-		}
-		
-		if (name != null) {
-            return name;
-        }
-        return null;     
-	}
 
 	public static void main(String[] args) {
-		s("str");
+		new C1().f = 4.3;
 	}
 
 }
